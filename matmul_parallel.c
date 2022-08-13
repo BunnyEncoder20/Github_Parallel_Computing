5a49122390529be1b8a7296c70e30ca4d86a11fc
BunnyEncoder20 <103634057+BunnyEncoder20@users.noreply.github.com>
2022-08-13 10:13:33 +0530

    Created Parallel Matrix Multiplication - leman approach

---
+++ matmul_parallel.c
@@ -0,0 +1,171 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<omp.h>
+
+//write parallel executing code for matrix multiplication
+
+//generating random matric using malloc
+
+#define maxi 100;
+
+#define mini 1;
+
+/*
+int** srand_square_matrix(int dimensions){
+	int** matrix  = malloc(dimensions*sizeof(int*));
+
+	for(int i=0; i<dimensions; i++)
+		matrix[i] = malloc(dimensions*sizeof(int));
+
+	//making 2D array of pointers completed
+
+	//generating a random seed each time
+	srand(time(0));
+
+	//genrerating the matrix
+	//notice how the parallel for block of pragma is structured :-
+	#pragma omp parallel for
+	for(int i=0; i<dimensions; i++)
+	{
+		for(int j=0; j<dimensions; j++)
+		{
+			matrix[i][j] = rand() % maxi + mini;
+		}
+	}
+
+	return matrix;
+}
+
+
+int sequential_multiplication(int** matA, int** matB, int dimension)
+{
+	int** matC = malloc(dimension*sizeof(int*));
+
+	for(int i=0; i<dimension; i++)
+	{
+		for(int j=0; j<dimension; j++)
+		{
+			for(int k=0;k<dimension; k++)
+			{
+				matC[i][j] += matA[i][k]*matB[k][j];
+			}
+		}
+	}
+
+	printf("\n\nMatrix C : \n");
+	for(int i=0; i<dimension; i++)
+	{
+		for(int j=0; j<dimension; j++)
+		{
+			printf("%d ",matC[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int parallel_multiplication(int** matA, int** matB, int dimension)
+{
+	int** matC = malloc(dimension*sizeof(int*));
+
+	#pragma omp parallel for
+	for(int i=0; i<dimension; i++)
+	{
+		for(int j=0; j<dimension; j++)
+		{
+			for(int k=0; k<dimension; k++)
+			{
+				matC[i][j] += matA[i][k]*matB[k][j];
+			}
+		}
+	}
+
+	printf("\n\nThe Kooler Matrix C : \n");
+	for(int i=0; i<dimension; i++)
+	{
+		for(int j=0; j<dimension; j++)
+		{
+			printf("%d ",matC[i][j]);
+		}
+		printf("\n");
+	}
+}
+*/
+
+int matgen(int dimensions)
+{
+	srand(time(0));
+	int matrix[dimensions][dimensions];
+
+	#pragma omp parllel for
+	for(int i=0; i<dimensions; i++)
+	{
+		for(int j=0; j<dimensions; j++)
+		{
+			matrix[i][j] = rand() % maxi + mini;
+		}
+	}
+
+	return matrix;
+}
+
+int main()
+{
+	int size;
+	printf("Enter the dimensions of the square matrix : \n");
+	scanf("%d",&size);
+
+	omp_set_num_threads(6);
+
+	int matA = matgen(size);
+	int matB = matgen(size);
+
+	printf("\n\nMatix A\n");
+	for(int i=0; i<size; i++)
+	{
+		for(int j=0; j<size; j++)
+		{
+			printf("%d ",matA[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\n\nMatrix B\n");
+	for(int i=0; i<size; i++)
+	{
+		for(int j=0; j<size; j++)
+		{
+			printf("%d ",matB[i][j]);
+		}
+		printf("\n");
+	}
+
+	#pragma omp parallel for
+	#pragma omp parallel for
+	int matC[size][size]
+	for(int i=0; i<size; i++)
+	{
+		for(int j=0; j<size; j++)
+		{
+			for(int k=0; k<size; k++)
+			{
+				matC[i][j] += matA[i][k]*matB[k][j];
+			}
+		}
+	}
+
+	printf("\n\nThe Kooler Matrix C : \n");
+	for(int i=0; i<size; i++)
+	{
+		for(int j=0; j<size; j++)
+		{
+			printf("%d ",matC[i][j]);
+		}
+		printf("\n");
+	}
+//	sequential_multiplication(matA, matB, dimensions);
+//	parallel_multiplication(matA, matB, dimensions);
+
+
+	return 0;
+}
